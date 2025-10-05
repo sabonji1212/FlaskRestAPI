1044ea1d5e51ab3e9e4d083029cce9467d6b3e2c
unknown <sbnj.tamo@gmail.com>
2025-10-05 15:11:15 +0100

    first ver

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask ,request 
+import uuid
+from db import stores,items
+app = Flask(__name__)
+
+
+@app.get('/store')
+def get_stores():
+    return {'stores': list(stores.values())}
+
+@app.post('/store')
+def create_store():
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = { **store_data, 'id': store_id }
+    stores[store_id] = store
+    return store , 201
+
+@app.post('/item')
+def create_item_in_store(name):
+        item_data = request.get_json() 
+        if item_data["store_id"] not in stores:
+            return {'message': 'store not found'}, 404
+        
+        item_id = uuid.uuid4().hex
+        item = { **item_data, 'id': item_id }
+        items[item_id] = item
+        
+        return item , 201
+
+
+@app.get('/item')
+def get_all_items():
+    return {'items': list(items.values())} 
+
+@app.get('/store/<string:store_id>')
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {'message': 'store not found'}, 404
+
+
+@app.get('/item/<string:item_id>')
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:    
+        return {'message': 'item not found'}, 404   
+    
